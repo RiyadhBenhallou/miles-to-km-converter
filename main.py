aa9d41ab5c876adfecab02508eec59b6127b3563
RiyadhBenhallou <157654216+RiyadhBenhallou@users.noreply.github.com>
2024-08-16 18:23:02 +0000

    finished project

---
+++ main.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+
+window =tk.Tk()
+window.title('Miles to Km Converter')
+window.minsize()
+window.config(padx=10, pady=10)
+
+
+input = tk.Entry()
+input.grid(column=1, row=0)
+
+miles = tk.Label(text='Miles')
+miles.grid(column=2, row=0)
+
+is_equal_to = tk.Label(text='is Equal to')
+is_equal_to.grid(column=0, row=1)
+
+result = tk.Label(text=0)
+result.grid(column=1, row=1)
+
+km = tk.Label(text='Km')
+km.grid(column=2, row=1)
+
+def calculate():
+  miles = int(input.get())
+  km = miles * 1.60934
+  result.config(text=km)
+
+button = tk.Button(text='Calculate', command=calculate)
+button.grid(column=1, row=2)
+
+
+
+
+
+
+window.mainloop()
